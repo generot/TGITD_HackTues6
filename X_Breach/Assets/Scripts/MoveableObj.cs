5bb3032f91ff9f01f33596ca75f1d35158810791
Anomalouss <anomalouss247@gmail.com>
2020-10-02 18:18:33 +0300

    Added gravitational simulation

---
+++ X_Breach/Assets/Scripts/MoveableObj.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableObj : MonoBehaviour
+{
+    public float mass;
+    private void Start() { mass = 50f; }
+}
