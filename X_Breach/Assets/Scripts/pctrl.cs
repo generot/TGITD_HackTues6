0df9285508f5e90abab97a1d35ca6610a2a6ddef
Venelin <weedfox8@gmail.com>
2020-10-02 20:10:16 +0300

    Added idle animation and Character

--- X_Breach/Assets/Scripts/pctrl.cs
+++ X_Breach/Assets/Scripts/pctrl.cs
@@ -5,17 +5,8 @@ using UnityEngine;
 public class pctrl : MonoBehaviour
 {
     Rigidbody2D rb;
-<<<<<<< HEAD
-
-    public float distToCover = 0.08f, jumpForce = 6f, decFactor = 0.3f;
-    bool isGrounded = false;
-    public float move = 0;
-        private Animator anim;
-  
-=======
     BaseEntity b_entity;
     rcast rc;
->>>>>>> ef838f5110031873d671200f24cbe2bc95f35043
     void Start()
     {
         b_entity = new BaseEntity();
@@ -32,76 +23,27 @@ public class pctrl : MonoBehaviour
 
     void FixedUpdate()
     {
-<<<<<<< HEAD
-        anim = GetComponent<Animator>();  
-        HandleMovement(transform, distToCover);
-        Jump(jumpForce);
-        if (move == 0)
-        {
-            anim.SetBool("IsRunning", false);
-        }
-        else
-        {
-            anim.SetBool("IsRunning", true);
-        }
-
-
-
-
-        
-
-
-=======
         GetComponent<Transform>().position = HandleMovement(transform);
         HandleJump(rb);
->>>>>>> ef838f5110031873d671200f24cbe2bc95f35043
     }
 
     public Vector2 HandleMovement(Transform pos)
     {
         Vector2 _pos = pos.position;
 
-
         if (Input.GetKey(KeyCode.A))
-<<<<<<< HEAD
-            if (isGrounded)
-            {
-                move = -1;
-                _pos.x -= ds;
-            }
-            else _pos.x -= ds * decFactor;
-
-        if (Input.GetKey(KeyCode.D))
-            if (isGrounded) {
-                move = 1;
-                _pos.x += ds; 
-            
-            
-            }
-            else _pos.x += ds * decFactor;
-
-
-        GetComponent<Transform>().position = _pos;
-        move = 0;
-=======
             _pos.x = b_entity.Move("Left", pos.position.x);
 
         if (Input.GetKey(KeyCode.D))
             _pos.x = b_entity.Move("Right", pos.position.x);
 
         return _pos;
->>>>>>> ef838f5110031873d671200f24cbe2bc95f35043
     }
 
     public void HandleJump(Rigidbody2D _rb)
     {
-<<<<<<< HEAD
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
-            rb.velocity = Vector2.up * ds; 
-=======
         if (Input.GetKey(KeyCode.Space))
             b_entity.Jump(_rb);
->>>>>>> ef838f5110031873d671200f24cbe2bc95f35043
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -114,8 +56,4 @@ public class pctrl : MonoBehaviour
         if (collision.collider.tag == "Ground")
             b_entity.isGrounded = false;
     }
-
-   
-
-
 }
--- X_Breach/Assets/Scripts/rcast.cs
+++ X_Breach/Assets/Scripts/rcast.cs
@@ -2,28 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rcast
+public class rcast : MonoBehaviour
 {
     public int units = 7;
     public void shoot(Transform transform, Vector3 mousepos)
     {
-<<<<<<< HEAD
-        if (Input.GetMouseButton(0))
-            shoot();
-     
-    }
-    void shoot()
-    {
-        Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        RaycastHit2D col = Physics2D.Raycast(dir, new Vector2(dir.x + units, dir.y));
 
-        if (col)
-=======
-        Vector2 dir = mousepos-transform.position;
+        Vector2 dir = mousepos - transform.position;
         Vector2 normalize = Vector2.ClampMagnitude(dir, 0.1f);
         RaycastHit2D col = Physics2D.Raycast(transform.position, normalize);
-        if (col) {
->>>>>>> 79b0abf3c21b32fae92b5643243a8a584550d3db
+        if (col)
+        {
+
             Debug.Log(col.transform.name);
+        }
     }
 }
