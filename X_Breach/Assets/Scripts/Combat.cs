e364e0ead37ee0f13e5c9b53b92f2f972b318344
idkmandocta <gosh19162@gmail.com>
2020-10-04 19:35:19 +0300

    combat animation

--- X_Breach/Assets/Scripts/Combat.cs
+++ X_Breach/Assets/Scripts/Combat.cs
@@ -8,18 +8,21 @@ public class Combat : MonoBehaviour
     public float attackRange;
     public LayerMask hostile;
     public int dmg;
-
+    public Animator animator;
     void Update()
     {
+
         if (Input.GetMouseButtonDown(1))
         {
             Melee();
+
         }
     }
     void Melee()
     {
+        animator.SetTrigger("attack");
         Collider2D[] entity_array = Physics2D.OverlapCircleAll(point.position, attackRange, hostile);
-        foreach(Collider2D Enemy in entity_array)
+        foreach (Collider2D Enemy in entity_array)
         {
             Debug.Log("we hit " + Enemy);
             Enemy.gameObject.GetComponent<Enemy>().b_entity.TakeDamage(dmg);
--- X_Breach/Assets/Scripts/rcast.cs
+++ X_Breach/Assets/Scripts/rcast.cs
@@ -4,31 +4,29 @@ using UnityEngine;
 
 public class rcast : MonoBehaviour
 {
-    public float units = 7f;
+    public float units;
     public int damage;
     public Transform sPoint;
+    //public LayerMask Target;
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            StartCoroutine (shoot(sPoint, Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+            StartCoroutine(shoot(sPoint, Vector2.right));
         }
     }
-    IEnumerator shoot(Transform transform, Vector3 mousePos)
-    {
-        Vector2 dir = mousePos - transform.position;
-        mousePos = Vector2.ClampMagnitude(dir, 1f);
-
-        RaycastHit2D enemyInf = Physics2D.Raycast(transform.position, mousePos, units);
-        if (enemyInf) {
+    IEnumerator shoot(Transform transform, Vector2 Pos)
+    { 
+        RaycastHit2D enemyInf = Physics2D.Raycast(transform.position, Pos, units);
+        if (enemyInf && enemyInf.collider.CompareTag("Enemy")) {
             Enemy hostile = enemyInf.transform.GetComponent<Enemy>();
             if (hostile != null)
             {
-                hostile.gameObject.GetComponent<Enemy>().b_entity.TakeDamage(damage);
-                Debug.Log(hostile.gameObject.GetComponent<Enemy>().b_entity.health);
+                hostile.b_entity.TakeDamage(damage);
+                Debug.Log(hostile.b_entity.health);
                 yield return 0; //for now because its a coroutine, but later because it will be an animation :)
 
-                if (hostile.gameObject.GetComponent<Enemy>().b_entity.health <= 0)
+                if (hostile.b_entity.health <= 0)
                 {
                     yield return 0; //same as ^
                     Destroy(hostile.gameObject, 0);
